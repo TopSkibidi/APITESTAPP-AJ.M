b4a5931b0c4abfa6679ed6191c6edc5de6924251
TopSkibidi <65011212058@msu.ac.th>
2024-08-07 10:40:43 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,21 @@ const app2 = express();
 const port1 = 3000;
 const port2 = 4000;
 
+const htmlContent = (port) => `
+  <html>
+    <body>
+      <h1>Hello World from port ${port}</h1>
+      <button onclick="window.location.href='https://github.com/TopSkibidi/APITESTAPP'">Go to GitHub</button>
+    </body>
+  </html>
+`;
+
 app1.get('/', (req, res) => {
-  res.send('Hello World from port 3000');
+  res.send(htmlContent(port1));
 });
 
 app2.get('/', (req, res) => {
-  res.send('Hello World from port 4000');
+  res.send(htmlContent(port2));
 });
 
 app1.listen(port1, () => {
